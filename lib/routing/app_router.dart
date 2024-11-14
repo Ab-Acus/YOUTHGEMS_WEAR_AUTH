b619012e7b26bccc72e8d26772e2f26236ddb13d
lauraGi <97524756+lauraGi@users.noreply.github.com>
2024-11-14 19:10:06 +0100

    fix: updated the app router class to properly manage routing to authentication and success pages.

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -1,23 +1,45 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../pages/authentication_page.dart';
 import '../pages/not_found_page.dart';
 import '../pages/success_page.dart';
 
-class AppRouter{
+class AppRouter {
   static final router = GoRouter(
-    errorBuilder: ((context, state) => const NotFoundPage()),
-    routes: [
+    errorBuilder: (context, state) =>
+        const NotFoundPage(), // Handle 404s globally
+    routes: <RouteBase>[
+      // Redirect the root path '/' to the 'not_found' path
+      GoRoute(
+        path: '/',
+        redirect: (context, state) => '/not_found',
+      ),
+
+      // Not found page route
+      GoRoute(
+        path: '/not_found',
+        builder: (BuildContext context, GoRouterState state) {
+          return const NotFoundPage();
+        },
+      ),
+
+      // Authentication request route
       GoRoute(
         path: '/:auth_request',
-        builder: (context, state) {
-          final authRequest = state.pathParameters['auth_request'].toString();
+        builder: (BuildContext context, GoRouterState state) {
+          final authRequest = state.pathParameters['auth_request']!.toString();
           return AuthenticationPage(authRequest: authRequest);
         },
       ),
+
+      // Success page route
       GoRoute(
-          path: "/:auth_request/success",
-          builder: (context, state) => const SuccessPage()),
+        path: '/:auth_request/success',
+        builder: (BuildContext context, GoRouterState state) {
+          return const SuccessPage();
+        },
+      ),
     ],
   );
 }
